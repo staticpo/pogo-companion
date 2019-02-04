db0ab747f3540451268a0270d4e4aa494d1c4017
staticpo <vazquezjorge@gmail.com>
2019-02-03 21:03:49 -0300

    More testing

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios'
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default class App extends React.Component {
+
+  constructor () {
+    super()
+    this.state = {
+      pokemon: ''
+    }
+
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick () {
+    axios.get('https://pokeapi.co/api/v2/pokemon/1')
+    .then(response => { 
+      console.log(response.data.name);
+      return response;
+    })
+    .then(response => this.setState({pokemon: response.data.name}))
+  }
+
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>{this.state.pokemon}</Text>
+        <Button title="This is the title" className='button' onClick={this.handleClick} onPress={this.handleClick}></Button>
       </View>
     );
   }
